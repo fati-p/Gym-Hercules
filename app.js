3fcf964137be39856be4cf178d047baead4e13c5
JGALINDO93 <116293351+JGALINDO93@users.noreply.github.com>
2024-06-29 02:29:13 -0600

    final

--- app.js
+++ app.js
@@ -1,20 +1,81 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mysql = require("mysql");
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'registro'
+    });
 
-// Configurar el directorio de archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+// Configurar el motor de vistas EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'vistas'));
 
-// Configurar las vistas
-app.set('vistas', path.join(__dirname, 'vistas'));
-app.set('vistas engine', 'html');
+// Configurar PARA LAS URL
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-// Rutas
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+app.use(express.static('public'));
 
-// Puerto de la aplicación
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+    res.render('index'); // Renderiza el archivo index.ejs
 });
+
+// Ruta para acerca.html
+app.get('/acerca.html', (req, res) => {
+    res.render('acerca'); // Renderiza el archivo acerca.ejs
+});
+
+// Otras rutas para las demás páginas
+app.get('/inicio.html', (req, res) => {
+    res.render('inicio'); // Renderiza el archivo inicio.ejs
+});
+
+app.get('/index.html', (req, res) => {
+    res.render('index'); // Renderiza el archivo index.ejs
+});
+
+app.get('/BLOG.HTML', (req, res) => {
+    res.render('BLOG'); // Renderiza el archivo BLOG.ejs
+});
+
+//CONFIGURACION DEL FORMULARIO //
+
+app.get('/CONTACTO.html', (req, res) => {
+    res.render('CONTACTO'); // Renderiza el archivo CONTACTO.ejs
+});
+
+app.get("/",function(req,res){
+    res.render("CONTACTO");
+});
+
+app.post("/validar", function(req,res){
+    const datos = req.body;
+
+    let usuario = datos.txt;
+    let contraseña = datos.pass;
+
+    let registrar = "INSERT INTO usuarios(usuario,contraseña) VALUES ('"+usuario+"','"+contraseña+"')";
+
+    conexion.query(registrar,function(error){
+        if(error){
+            throw error;
+            }
+            else
+            {
+                console.log("datos guardados con éxito");
+            }
+      });
+});
+
+// Configurar el puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+module.exports = app;
